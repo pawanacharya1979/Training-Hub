3090caf3731af6bfedb2b4494ed84d9ee372cbe8
Sunera <pawanacharya1979@gmail.com>
2019-02-26 14:40:50 +0530

    Gettting firstname

--- django/priya/app1/urls.py
+++ django/priya/app1/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
      url(r'^$', views.index, name='index'),
      url(r'^login/$', views.login1, name='login1'),
      url(r'^course/$', views.courses, name='courses'),
-    url(r'^aboutus/$', views.about, name='about'),
-    url(r'^contact/$', views.contactus, name='contactus'),
-    url(r'^registration/$', views.regform, name='regform'),
+     url(r'^aboutus/$', views.about, name='about'),
+     url(r'^contact/$', views.contactus, name='contactus'),
+     url(r'^registration/$', views.regform, name='regform'),
      url(r'^logout/$',auth_views.logout,{'next_page': 'login1'}, name='logout'),
      url(r'^get_result/$', views.response, name='response'),
 
